6c261f174196fb219eb9aeef1890664ed6527074
Jose Carrillo <jose@amigocloud.com>
2015-08-26 10:37:16 -0500

    Acepta potencias como input + Setting para nro de jugadores
    Antes solo se podía recibir velocidad lineal y angular. Ahora se puede
    recibir las potencias de los motores derecho e izquierdo. Internamente
    lo que se hace es usar los conceptos de odometría para transformar las
    potencias de los motores en velocidad linear y angular.
    
    También se agregó un setting en user_code.settings para cambiar el número
    de jugadores por equipo:
        BT = 3
        RT = 3
    También se puede dejar sin poner este setting y por defecto habrá 3
    jugadores por equipo.

---
+++ lib/settings.py
@@ -0,0 +1,16 @@
+# RT_CMD and BT_CMD are how you command your team
+# RT stands for red team and BT stands for blue team
+# CMD stands for command
+# There are two options:
+#     CMD_TYPE_VEL: You must provide linear and angular velocities
+#     CMD_TYPE_POW: You must provide left and right motor powers
+
+CMD_TYPE_VEL = 'velocidad'
+CMD_TYPE_POW = 'potencia'
+
+RT_CMD = CMD_TYPE_VEL
+BT_CMD = CMD_TYPE_POW
+
+# Set the number of players
+RT = 3
+BT = 3
