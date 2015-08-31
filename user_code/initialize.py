b8ae1d9a5df185790c1df7f7c8b18ae942191743
Jose Carrillo <jose@amigocloud.com>
2015-08-31 10:55:30 -0500

    Corregir la posición inicial de los robots
    Antes empezaban todos mirando en la misma dirección: a un lado de
    la cancha. Ahora los robots se miran frente a frente, cada equipo
    mirando al arco contrario.

--- user_code/initialize.py
+++ user_code/initialize.py
@@ -1,8 +1,7 @@
 def reset_red_team(red_team):
     red_team_positions = [
-        [0, 10, 3.5],
+        [10, -10, 3.5],
         [10, 10, 3.5],
-        # [-10, 10, 3.5]
     ]
 
     for robot, pos in zip(red_team, red_team_positions):
@@ -10,13 +9,13 @@ def reset_red_team(red_team):
         # This eliminates any momentum, [0, 0, 0] doesn't work
         robot.setAngularVelocity([0.01] * 3)
         robot.worldPosition = pos
+        robot.alignAxisToVect([-1, 0, 0], 1, 1)
     
 
 def reset_blue_team(blue_team):
     blue_team_positions = [
-        [0, -10, 3.5],
-        [10, -10, 3.5],
-        # [-10, -10, 3.5]
+        [-10, 10, 3.5],
+        [-10, -10, 3.5],
     ]
 
     for robot, pos in zip(blue_team, blue_team_positions):
@@ -24,6 +23,7 @@ def reset_blue_team(blue_team):
         # This eliminates any momentum, [0, 0, 0] doesn't work
         robot.setAngularVelocity([0.01] * 3)
         robot.worldPosition = pos
+        robot.alignAxisToVect([1, 0, 0], 1, 1)
 
 
 def reset_ball(ball):
