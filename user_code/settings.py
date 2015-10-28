655ace25aa145c6a381437540b2acc0cfa4039b0
Jose Carrillo <jose@amigocloud.com>
2015-10-28 00:49:37 -0200

    Mandar posicion de bola dentro del arco al hacer gol

--- user_code/settings.py
+++ user_code/settings.py
@@ -1,35 +1,40 @@
 from lib.settings import *
 
 # Set the number of players
-RT = 1
-BT = 1
+RT = 3
+BT = 3
 
 red_team_positions = [
-    # [10, -10],
-    # [10, 10],
+    [10, -10],
+    [10, 10],
     [10, 0],
 ]
 
 red_team_orientations = [
-    [-1, 0, 0]
+    [-1, 0, 0],
+    [-1, 0, 0],
+    [-1, 0, 0],
 ]
 
 blue_team_positions = [
-    # [-10, 10],
-    # [-10, -10],
-    [-10, -50],
+    [-10, 10],
+    [-10, -10],
+    [-10, 0],
 ]
 
 blue_team_orientations = [
-    [1, 0, 0]
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0],
 ]
 
-ball_position = [0, -35]
+ball_position = [0, 0]
 
-THIS_SERVER = ('0.0.0.0', 9001)
-STRATEGY_SERVERS = [('0.0.0.0', 9002)]
+THIS_SERVER = ('0.0.0.0', 9003)
+STRATEGY_SERVERS = [('0.0.0.0', 9002),
+                    ('0.0.0.0', 9004)]
 
-VISION_LATENCY = 500           # milliseconds
-VISION_ACCURACY = 0.5
-VISION_ANGLE_ACCURACY = 2
-VISION_LOSE_RATE = 0.05  # percentaje
+VISION_LATENCY = 0           # milliseconds
+VISION_ACCURACY = 0.0   # error in cm
+VISION_ANGLE_ACCURACY = 0.0     # error in degrees
+VISION_LOSE_RATE = 0.0  # percentaje
